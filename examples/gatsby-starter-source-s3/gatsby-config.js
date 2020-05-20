fdac799a6470ab25c50025020a40a0bf6088aba6
Robin Metral <robin.metral@sumup.com>
2020-05-20 17:44:20 +0200

    chore: add new bucket to example site

--- examples/gatsby-starter-source-s3/gatsby-config.js
+++ examples/gatsby-starter-source-s3/gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: `gatsby-starter-source-s3`
+    title: `gatsby-starter-source-s3`,
   },
   plugins: [
     {
@@ -11,13 +11,17 @@ module.exports = {
         aws: {
           accessKeyId: process.env.AWS_ACCESS_KEY_ID,
           secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-          region: process.env.AWS_REGION
+          region: process.env.AWS_REGION,
         },
-        buckets: ["gatsby-source-s3-example", "gatsby-source-s3-example-2"]
-      }
+        buckets: [
+          "gatsby-source-s3-example",
+          "gatsby-source-s3-example-2",
+          "gatsby-source-s3-continuation-token",
+        ],
+      },
     },
     // the sharp transformer and plugin are required to process images
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
-  ]
+    `gatsby-plugin-sharp`,
+  ],
 };
