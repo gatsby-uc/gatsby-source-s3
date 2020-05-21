63c97e7963bb34a4356fb31526dc03b5e7e7177b
Robin Metral <robin.metral@sumup.com>
2020-05-21 20:57:43 +0200

    feat: get signed URL only for image files in onCreateNode
    BREAKING CHANGE: the Url key doesn't exist on s3Object anymore. There is not a public URL for private buckets and file nodes are now sourced with pre-signed URLs that shouldn't be added to the schema.

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,6 +1,8 @@
 import { createRemoteFileNode } from "gatsby-source-filesystem";
 import AWS = require("aws-sdk");
 
+const s3 = new AWS.S3();
+
 const isImage = (key: string): boolean =>
   /\.(jpe?g|png|webp|tiff?)$/i.test(key);
 
@@ -24,7 +26,6 @@ export async function sourceNodes(
 
   // configure aws
   AWS.config.update(awsConfig);
-  const s3 = new AWS.S3();
 
   // get objects
   const getS3ListObjects = async (params: {
@@ -85,21 +86,11 @@ export async function sourceNodes(
     const objects = allBucketsObjects.reduce((acc, val) => acc.concat(val), []);
 
     // create file nodes
-    // todo touch nodes if they exist already
     objects?.forEach(async (object) => {
-      const { Key, Bucket } = object;
-      // get pre-signed URL
-      const Url = s3.getSignedUrl("getObject", {
-        Bucket,
-        Key,
-        Expires: 60,
-      });
-
       createNode({
         ...object,
-        Url,
         // node meta
-        id: createNodeId(`s3-object-${Key}`),
+        id: createNodeId(`s3-object-${object.Key}`),
         parent: null,
         children: [],
         internal: {
@@ -124,9 +115,16 @@ export async function onCreateNode({
 }) {
   if (node.internal.type === "S3Object" && node.Key && isImage(node.Key)) {
     try {
+      // get pre-signed URL
+      const url = s3.getSignedUrl("getObject", {
+        Bucket: node.Bucket,
+        Key: node.Key,
+        Expires: 60,
+      });
+
       // download image file and save as node
       const imageFile = await createRemoteFileNode({
-        url: node.Url,
+        url,
         parentNodeId: node.id,
         store,
         cache,
